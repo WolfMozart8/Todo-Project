81c63ca2ef0613dd2ae97bfde89c05dae2ece2c9
felipe <riopipe8@gmail.com>
2023-01-11 00:31:14 -0300

    Restart project

--- src/createTask.js
+++
@@ -1,7 +0,0 @@
-import { taskDom } from "./dom";
-
-export const NewToDo = function (name, date) {
-this.name = name;
-this.date = date;
-this.tasks = [];
-}
--- src/index.js
+++ src/index.js
@@ -4,31 +4,3 @@ import * as create from "./createTask";
 import date from "./dates";
 
 
-const dameTasks = {
-    name: "Demo project", 
-    date: date(),
-    tasks: ["Make the bed", "Drink coffe", "Do notthing as usual"]
-}
-dom(date(), dameTasks.tasks);
-const hola = new create.NewToDo("projrcect 1", date());
-
-const addInput = document.querySelector(".add-input");
-const btn = document.querySelector(".add-btn");
-const deleteBtn = document.querySelectorAll(".delete-task");
-
-btn.addEventListener("click", () => {
-    if (addInput.value != "") {
-        taskDom(addInput.value);
-        dameTasks.tasks.push(addInput.value);
-        addInput.value = "";
-    }
-    else {
-        console.log("MMMMmmm ");
-    }
-})
-
-deleteBtn.forEach(e => {
-    e.addEventListener("click", (a) => {
-        e.parentElement.parentElement.remove();
-    })
-})
